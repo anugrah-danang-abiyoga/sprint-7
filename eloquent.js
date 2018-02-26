183903bb3ae3723d04fb968e79c6b3c23515dc46
anugrah-danang <danangabiyoga26@gmail.com>
2018-02-26 13:12:18 +1300

    initial commit

---
+++ eloquent.js
@@ -0,0 +1,18 @@
+/* Minimum */
+
+function min(num1, num2) {
+    if (num1 > num2) {
+        return num2;
+    } else if (num1 < num2) {
+        return num1; 
+    } else {
+        return num1;
+    }
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+
+
