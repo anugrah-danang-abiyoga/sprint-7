92b2d4ea74cda48df2859f9476db7bfd7d452c31
anugrah-danang <danangabiyoga26@gmail.com>
2018-02-26 13:27:15 +1300

    initial commit

--- eloquent.js
+++ eloquent.js
@@ -16,3 +16,23 @@ console.log(min(0, -10));
 // → -10
 
 
+/* Recursion */
+
+function isEven(num) {
+    if (num == 0) {
+        return true;
+    } else if (num == 1) {
+        return false;
+    } else if (num < 0) {
+        isEven (num + 2);
+    } else {
+        (num - 2);
+    }
+}
+ 
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
